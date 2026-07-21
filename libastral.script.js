fd7880ecba4fdc08668b24ab51d47dfd3ab5bd8f
Astral <132731805+astralsc@users.noreply.github.com>
2026-07-22 01:39:02 +0800

    Update libastral.script.js

--- libastral.script.js
+++ libastral.script.js
@@ -1,16 +1,6 @@
-const Host = '127.0.0.1';
-const Port = '9339';
-
 Interceptor.attach(Process.getModuleByName("libc.so").findExportByName('getaddrinfo'), {
     onEnter(args) {
-        const arg0 = args[0];
-        const arg1 = args[1];
-        const AskForHost = arg0.readUtf8String();
-        const AskForPort = arg1.readUtf8String();
-
-        console.log(`${AskForHost}:${AskForPort} > ${Host}:${Port}!`);      
-
-        arg0.writeUtf8String(Host);
-        arg1.writeUtf8String(Port);
+        args[0].writeUtf8String('127.0.0.1') // Host
+        args[1].writeUtf8String('9339') // Port
     }
-});
+})
